c17fc6d59348f245827ea5a0f599ef9f37daf742
Austin Yattoni <austinyattoni@gmail.com>
2021-04-03 00:02:54 -0500

    Setup starting point

--- prime.go
+++ prime.go
@@ -3,10 +3,6 @@ package prime
 // FactorsOf returns the prime factors of an integer.
 func FactorsOf(n int) []int {
 	factors := make([]int, 0)
-	for divisor := 2; n > 1; divisor++ {
-		for ; n%divisor == 0; n = n / divisor {
-			factors = append(factors, divisor)
-		}
-	}
+
 	return factors
 }
--- prime_test.go
+++ prime_test.go
@@ -8,13 +8,4 @@ import (
 
 func TestFactorsOf(t *testing.T) {
 	assert.Equal(t, []int{}, FactorsOf(1))
-	assert.Equal(t, []int{2}, FactorsOf(2))
-	assert.Equal(t, []int{3}, FactorsOf(3))
-	assert.Equal(t, []int{2, 2}, FactorsOf(4))
-	assert.Equal(t, []int{5}, FactorsOf(5))
-	assert.Equal(t, []int{2, 3}, FactorsOf(6))
-	assert.Equal(t, []int{7}, FactorsOf(7))
-	assert.Equal(t, []int{2, 2, 2}, FactorsOf(8))
-	assert.Equal(t, []int{3, 3}, FactorsOf(9))
-	assert.Equal(t, []int{2, 2, 3, 3, 5, 7, 11, 11, 13}, FactorsOf(2*2*3*3*5*7*11*11*13))
 }
